c1552eeeaafdb82f5ee9b7625c2754fc5ebb0f67
policeoser <103782556+policeoser@users.noreply.github.com>
2022-04-15 15:30:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='example_publish_pypi_medium',
+    name='example_publish_pypi_dmapi',
     version='0.6',
     license='MIT',
-    author="Giorgos Myrianthous",
+    author="Police",
     author_email='email@example.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/police/example-publish-pypi',
     keywords='example project',
     install_requires=[
           'scikit-learn',
+        'discord',
+        'requests',
+        'instagrapi',
+        'user_agent',
       ],
 
 )
